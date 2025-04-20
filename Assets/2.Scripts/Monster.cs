f7e77d54619258583849f2a800bcfa90d8ef336e
dhghgh189 <dhghgh189@gmail.com>
2025-04-20 22:43:24 +0900

    [Feat] 박스 레이어 추가, 몬스터 공격 상태 전환 추가

--- Assets/2.Scripts/Monster.cs
+++ Assets/2.Scripts/Monster.cs
@@ -8,12 +8,17 @@ public class Monster : MonoBehaviour
     // 몬스터의 상태 정의
     public enum EMonsterState { Move, Attack, Dead }
 
-    [SerializeField] private float moveSpeed;       // 이동 속도
-    private EMonsterState curState;                 // 현재 상태
-    private Animator anim;                          // Animator 캐시
+    [SerializeField] private float moveSpeed;           // 이동 속도
+    [SerializeField] private float raycastRange;        // 레이캐스트 사거리
+    [SerializeField] private LayerMask whatIsTarget;    // 레이캐스트 타겟을 구분하기 위한 layerMask 
+    [SerializeField] private Transform rayOrigin;       // 레이캐스트 시작점 트랜스폼
+    private EMonsterState curState;                     // 현재 상태
+    private Animator anim;                              // Animator 캐시
 
     private int isAttackingHash = Animator.StringToHash("IsAttacking");
     private int isDeadHash = Animator.StringToHash("IsDead");
+
+    private RaycastHit2D hit;
 
     private void Awake()
     {
@@ -50,9 +55,30 @@ public class Monster : MonoBehaviour
     {
         // 매 프레임 왼쪽으로 이동 
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+
+        // 감지되는 물체가 있으면 Attack 상태로 변경 (임시)
+        Debug.DrawRay(rayOrigin.position, Vector2.left * raycastRange, Color.red);
+        hit = Physics2D.Raycast(rayOrigin.position, Vector2.left, raycastRange, whatIsTarget);
+        if (hit.collider != null)
+        {
+            curState = EMonsterState.Attack;
+            return;
+        }
     }
 
     protected virtual void UpdateAttack()
+    {
+        // 감지되는 물체가 없으면 Move 상태로 변경
+        Debug.DrawRay(rayOrigin.position, Vector2.left * raycastRange, Color.red);
+        hit = Physics2D.Raycast(rayOrigin.position, Vector2.left, raycastRange, whatIsTarget);
+        if (hit.collider == null)
+        {
+            curState = EMonsterState.Move;
+            return;
+        }
+    }
+
+    protected virtual void OnAttack()
     {
 
     }
